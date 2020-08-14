7e60b0a5a36691980e6cd567520f3193479de1b0
Anna-2800 <69672564+Anna-2800@users.noreply.github.com>
2020-08-14 14:25:49 +0400

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -5,31 +5,32 @@ public class Calculator {
     public static void main (String [] args) {
         System.out.println("Введите выражение c двумя целыми числами ");
         Scanner scan = new Scanner(System.in);
-
-        if (scan.hasNextInt()) {
+        
+        if (scan.hasNextInt()) { //если введены арабские цифры
             int num1 = NumScanner.num(scan);
             char operation = OperationScanner.operation(scan);
             int num2 = NumScanner.num(scan);
             int result = calculate(num1, num2, operation);
-            if (result == 101) {
+            if (result == 101) { //проверка результата (?)
             } else {
                 System.out.println("Результат " + result);
             }
         }
-
-        else {
+        
+        else { // если введены римские цифры
             int num1 = NumRome.arabicNum(scan);
             char operation = OperationScanner.operation(scan);
             int num2 = NumRome.arabicNum(scan);
             int result = Calculator.calculate(num1, num2, operation);
-            if (result==101) {
+            if (result==101) { //(?)
             } else {System.out.println("Результат " + NumRome.romanNum(result));
             }
-    }}
-
-    public static int calculate(int num1, int num2, char operation){
-        int result=101;
-        if (num1>=1 & num1<=10 & num2>=1 & num2<=10){
+    }
+    }
+    
+    public static int calculate(int num1, int num2, char operation){ //проведение арифметической операции
+        int result=101; //(?)
+        if (num1>=1 & num1<=10 & num2>=1 & num2<=10){ //диапазон допустимых значений введенных чисел
             switch (operation) {
                 case '+':
                     result = num1 + num2;
@@ -44,7 +45,7 @@ public class Calculator {
                     result = num1 / num2;
                     break;
                 default:
-                    System.out.println("Ошибка. Неверный тип операции. ");
+                    System.out.println("Ошибка. Неверный тип операции. "); 
             }
         } else {
             System.out.println("Ошибка диапазона. Принимаются числа от 1 до 10 включительно. ");
