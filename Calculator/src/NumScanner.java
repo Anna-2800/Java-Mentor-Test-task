aca31125f61ca188a81bf2bb8cd6256a2adc5bad
Anna-2800 <69672564+Anna-2800@users.noreply.github.com>
2020-08-14 14:43:00 +0400

    Update NumScanner.java

--- Calculator/src/NumScanner.java
+++ Calculator/src/NumScanner.java
@@ -4,10 +4,10 @@ public class NumScanner {
 
     public static int num (Scanner scan) {
         int num=0;
-        if (scan.hasNextInt()) {
+        if (scan.hasNextInt()) { //сканируем введеные числа
             num = scan.nextInt();
         } else {
-            System.out.print("Ошибка. Введен неверный символ");
+            System.out.print("Ошибка. Введен неверный символ. ");
         }
         return num;
     }
